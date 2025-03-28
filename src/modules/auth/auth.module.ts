b6afb18acdb849f71929a57fe7777fb415fffba9
Dener-Fernandes <dener.fernandes.oliveira@gmail.com>
2025-03-28 02:49:56 -0400

    feat: add signup feature returning jwt

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConfig } from 'src/config/jwt.config';
+import { ConfigType } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      inject: [jwtConfig.KEY],
+      useFactory: async (config: ConfigType<typeof jwtConfig>) => config,
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,22 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { AuthSignUpDto } from './dtos/auth-signup.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { UserDto } from './dtos/user.dto';
 import { plainToInstance } from 'class-transformer';
+import { ConfigType } from '@nestjs/config';
+import { jwtConfig } from 'src/config/jwt.config';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
+import { AuthResponseDto } from './dtos/auth-response.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
+    private jwtService: JwtService,
     @InjectRepository(User) private readonly userRepository: Repository<User>,
+    @Inject(jwtConfig.KEY) private config: ConfigType<typeof jwtConfig>,
   ) {}
 
-  async signUp(authSignUpDto: AuthSignUpDto): Promise<UserDto> {
+  async signUp(authSignUpDto: AuthSignUpDto): Promise<AuthResponseDto> {
     const tempUser = this.userRepository.create(authSignUpDto);
 
     const dbUser = await this.userRepository.save(tempUser);
 
-    return plainToInstance(UserDto, dbUser);
+    const user = plainToInstance(UserDto, dbUser);
+
+    return await this.jwtSign(user);
+  }
+
+  async jwtSign(user: UserDto): Promise<AuthResponseDto> {
+    const accessConfig = this.config.access;
+
+    const payload: JwtPayload = { sub: user.id };
+
+    const accessToken = this.jwtService.sign(
+      payload,
+      accessConfig?.signOptions,
+    );
+
+    return new AuthResponseDto(accessToken);
   }
 }
